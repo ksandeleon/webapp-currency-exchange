ad0f8c652fe3de1bcc7f211e874d8a5d365cff69
ksandeleon <ksanureeves1@gmail.com>
2025-02-21 17:55:50 +0800

    finish app functionality

--- src/components/converter-form.jsx
+++ src/components/converter-form.jsx
@@ -1,20 +1,68 @@
 import CurrencySelect from "./currency-select"
+import { useState } from "react"
+import { useEffect } from "react"
 
 const ConverterForm = () => {
+    const [amount, setAmount] = useState(100);
+    const [fromCurrency, setFromCurrency] = useState("USD");
+    const [toCurrency, setToCurrency] = useState("PHP");
+    const [result, setResult] = useState("");
+    const [isLoading, setIsLoading] = useState(false)
+
+    const handleSwapCurrency = () => {
+        setFromCurrency(toCurrency);
+        setToCurrency(fromCurrency);
+    }
+
+    const getExchangeRate = async () => {
+        const API_KEY = import.meta.env.VITE_API_KEY;
+        const API_URL = `https://v6.exchangerate-api.com/v6/6a1f69b79411634f142022e2/pair/${fromCurrency}/${toCurrency}`;
+
+        setIsLoading(true);
+
+        try {
+            const response = await fetch(API_URL);
+            if (!response.ok) {
+                throw new Error("Request failed");
+            }
+
+            const data = await response.json();
+            const rate = (data.conversion_rate * amount).toFixed(2);
+            console.log(rate);
+            setResult(`${amount} ${fromCurrency} = ${rate} ${toCurrency}`);
+
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+
+    const handleFromSubmit = (e) => {
+        e.preventDefault();
+        getExchangeRate();
+    }
+
+    useEffect(() => getExchangeRate, []);
+
   return (
-    <form className="converter-form">
+    <form className="converter-form" onSubmit={handleFromSubmit}>
             <div className="form-group">
                 <label className="form-label">Enter Amount</label>
-                <input type="number" className="form-input" required />
+                <input type="number" className="form-input" value={amount}onChange={e => setAmount(e.target.value)}  required />
             </div>
 
             <div className="form-group form-currency-group">
                 <div className="form-section">
                     <label className="form-label">From</label>
-                    <CurrencySelect />
+                    <CurrencySelect
+                        selectedCurrency={fromCurrency}
+                        handleCurrency={e => setFromCurrency(e.target.value)}
+                    />
                 </div>
 
-                <div className="swap-icon">
+                <div className="swap-icon" onClick={handleSwapCurrency}>
                     <svg width="16" viewBox="0 0 20 19" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M19.13 11.66H.22a.22.22 0 0 0-.22.22v1.62a.22.22 0 0 0 .22.22h16.45l-3.92 4.94a.22.22 0 0 0 .17.35h1.97c.13 0 .25-.06.33-.16l4.59-5.78a.9.9 0 0 0-.7-1.43zM19.78 5.29H3.34L7.26.35A.22.22 0 0 0 7.09 0H5.12a.22.22 0 0 0-.34.16L.19 5.94a.9.9 0 0 0 .68 1.4H19.78a.22.22 0 0 0 .22-.22V5.51a.22.22 0 0 0-.22-.22z"
@@ -25,12 +73,16 @@ const ConverterForm = () => {
 
                 <div className="form-section">
                     <label className="form-label">To</label>
-                    <CurrencySelect />
+                    <CurrencySelect
+                        selectedCurrency={toCurrency}
+                        handleCurrency={e => setToCurrency(e.target.value)}
+
+                    />
                 </div>
             </div>
 
-                <button type="submit" className="submit-button">Get Exchange Rate</button>
-                <p className="exchange-rate-result">1,000 USD = 83629,80 INR</p>
+                <button type="submit" className={`${isLoading ? "loading" : " ... "}submit-button`} >Get Exchange Rate</button>
+                <p className="exchange-rate-result">{isLoading ? "Fetching Exchange Rates.." : result}</p>
         </form>
   )
 }
--- src/components/currency-select.jsx
+++ src/components/currency-select.jsx
@@ -1,11 +1,37 @@
-const CurrencySelect = () => {
+// Array of currency codes
+const currencyCodes = [
+    "AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN",
+    "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL",
+    "BSD", "BTN", "BWP", "BYN", "BZD", "CAD", "CDF", "CHF", "CLP", "CNY",
+    "COP", "CRC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP",
+    "ERN", "ETB", "EUR", "FJD", "FKP", "FOK", "GBP", "GEL", "GGP", "GHS",
+    "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF",
+    "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD",
+    "JPY", "KES", "KGS", "KHR", "KID", "KMF", "KRW", "KWD", "KYD", "KZT",
+    "LAK", "LBP", "LKR", "LRD", "LSL", "LYD", "MAD", "MDL", "MGA", "MKD",
+    "MMK", "MNT", "MOP", "MRU", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN",
+    "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK",
+    "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR",
+    "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLE", "SLL", "SOS", "SRD",
+    "SSP", "STN", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY",
+    "TTD", "TVD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS", "VES",
+    "VND", "VUV", "WST", "XAF", "XCD", "XOF", "XPF", "YER", "ZAR", "ZMW",
+    "ZWL"
+];
+
+const CurrencySelect = ({selectedCurrency, handleCurrency}) => {
+    const countryCode = selectedCurrency.substring(0, 2);
+
   return (
     <div className="currency-select">
-        <img src="https://flagsapi.com/US/flat/64.png" alt="currrency country flag " />
-        <select className="currency-dropdown">
-            <option value="USD" selected>USD</option>
-            <option value="INR">INR</option>
-            <option value="NPR">NPR</option>
+        <img src={`https://flagsapi.com/${countryCode}/flat/64.png`} alt="currrency country flag " />
+        <select
+            onChange={handleCurrency}
+            className="currency-dropdown"
+            value={selectedCurrency}>
+            {currencyCodes.map(currency => (
+                <option key={currency} value={currency}>{currency}</option>
+            ))}
         </select>
     </div>
   )
